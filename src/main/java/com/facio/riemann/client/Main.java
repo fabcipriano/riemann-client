18b42ec3a0adefaeb96e2aaedc623ca96921f1bf
fabiano <fabiano@asimov>
2017-05-05 18:40:36 -0300

    initial commit

---
+++ src/main/java/com/facio/riemann/client/Main.java
@@ -0,0 +1,14 @@
+/*
+ */
+package com.facio.riemann.client;
+
+/**
+ *
+ * @author fabiano
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
