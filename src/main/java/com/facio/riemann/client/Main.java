12c8aad1ea90567aa4944166363e694f3986a4d2
Fabiano C. de Oliveira <fabcipriano@yahoo.com.br>
2017-05-05 19:48:45 -0300

    ignore and Test to send event to Riemann

--- src/main/java/com/facio/riemann/client/Main.java
+++ src/main/java/com/facio/riemann/client/Main.java
@@ -2,13 +2,30 @@
  */
 package com.facio.riemann.client;
 
+import io.riemann.riemann.client.RiemannClient;
+import java.io.IOException;
+
 /**
  *
  * @author fabiano
  */
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Send to riemann ...");
+        RiemannClient c = RiemannClient.tcp("localhost", 5555);
+        c.connect();
+        c.event().
+                service("console-java-client").
+                state("running").
+                metric(66.6).
+                tags("test", "java").
+                send().
+                deref(5000, java.util.concurrent.TimeUnit.MILLISECONDS);
+
+        c.query("tagged \"test\" and metric > 0").deref(); // => List<Event>;
+        c.close();     
+        
+        System.out.println("Event sent to Riemann.");
     }
 }
