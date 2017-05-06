cca742b759b9abc8037826cd55f4af13d0d8fb9d
Fabiano C. de Oliveira <fabcipriano@yahoo.com.br>
2017-05-06 11:24:54 -0300

    Logging log4j 2

--- src/main/java/com/facio/riemann/client/Main.java
+++ src/main/java/com/facio/riemann/client/Main.java
@@ -4,28 +4,41 @@ package com.facio.riemann.client;
 
 import io.riemann.riemann.client.RiemannClient;
 import java.io.IOException;
+import java.util.logging.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  *
  * @author fabiano
  */
 public class Main {
+    private static final Logger LOG = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Send to riemann ...");
-        RiemannClient c = RiemannClient.tcp("localhost", 5555);
-        c.connect();
-        c.event().
-                service("console-java-client").
-                state("running").
-                metric(66.6).
-                tags("test", "java").
-                send().
-                deref(5000, java.util.concurrent.TimeUnit.MILLISECONDS);
+    public static void main(String[] args) {
+        try {            
+            LOG.info("Send to riemann ...");
+            
+            RiemannClient c = RiemannClient.tcp("localhost", 5555);
+            c.connect();
+            c.event().
+                    service("console-java-client").
+                    state("running").
+                    metric(66.6).
+                    tags("test", "java").
+                    send().
+                    deref(5000, java.util.concurrent.TimeUnit.MILLISECONDS);
+            
+            LOG.info("Event sent to Riemann.");
 
-        c.query("tagged \"test\" and metric > 0").deref(); // => List<Event>;
-        c.close();     
-        
-        System.out.println("Event sent to Riemann.");
+            c.query("tagged \"test\" and metric > 0").deref(); // => List<Event>;
+            
+            LOG.info("Event found");
+            c.close();
+            
+            LOG.info("Riemann client closed.");        
+        } catch (Exception ex) {
+            LOG.error("failed to execute client", ex);
+        }
     }
 }
