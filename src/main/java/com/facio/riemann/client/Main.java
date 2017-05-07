adafe278a4f01f4003948851b0c9071513f64e6b
Fabiano C. de Oliveira <fabcipriano@yahoo.com.br>
2017-05-06 23:09:12 -0300

    use try with resources for Riemann client

--- src/main/java/com/facio/riemann/client/Main.java
+++ src/main/java/com/facio/riemann/client/Main.java
@@ -2,8 +2,10 @@
  */
 package com.facio.riemann.client;
 
+import io.riemann.riemann.Proto;
 import io.riemann.riemann.client.RiemannClient;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,29 +18,28 @@ public class Main {
     private static final Logger LOG = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        try {            
+        try (RiemannClient c = RiemannClient.tcp("localhost", 5555)) {
             LOG.info("Send to riemann ...");
             
-            RiemannClient c = RiemannClient.tcp("localhost", 5555);
             c.connect();
             c.event().
                     service("console-java-client").
                     state("running").
-                    metric(66.6).
+                    metric(99.9).
                     tags("test", "java").
                     send().
                     deref(5000, java.util.concurrent.TimeUnit.MILLISECONDS);
             
             LOG.info("Event sent to Riemann.");
 
-            c.query("tagged \"test\" and metric > 0").deref(); // => List<Event>;
+            List<Proto.Event> events = c.query("tagged \"test\" and metric > 0").deref(); // => List<Event>;
             
-            LOG.info("Event found");
-            c.close();
+            LOG.info("Event found, events={}", events);
             
-            LOG.info("Riemann client closed.");        
         } catch (Exception ex) {
             LOG.error("failed to execute client", ex);
+        } finally {
+            LOG.info("Riemann client closed.");        
         }
     }
 }
